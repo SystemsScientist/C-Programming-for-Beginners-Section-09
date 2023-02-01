0af2dedf421347f2f2bb32800c0f97415ef5fda7
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-02-01 06:56:41 -0600

    Adding Defining Functions files and directories

---
+++ Defining-Functions/main.c
@@ -0,0 +1,24 @@
+/*
+    Author: Matt Johnson
+    Subject: functions
+    Purpose: program illustrates multiple functions
+             with a function prototype
+    Date: 02/01/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void add();         // function prototype
+
+int main(void) {    // function header
+
+    add();          // function call
+
+    return 0;
+}
+
+void add() {        // function header
+
+    // statments;
+}
